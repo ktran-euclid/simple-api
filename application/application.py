60d63ccfcea5d2f9c3aafd9ede3c1a1976fc2110
Khoi Tran <ktran@euclidanalytics.com>
2018-01-18 00:17:01 -0800

    realizing need a separate async server for fetching data from sse

--- application/application.py
+++ application/application.py
@@ -1,8 +1,11 @@
 # all the imports
-import json
-import os
+
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+import json
+import os
+import pprint
+import sseclient
 
 app = Flask('ScoreApplication') # create the application instance :)
 app.config.from_object(__name__) # load config from this file , application.py
@@ -10,5 +13,20 @@ app.config.from_object(__name__) # load config from this file , application.py
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 @app.route('/students', methods=['GET'])
-def students():
+def ping():
+    """Test if server is alive"""
     return json.dumps({'hello': 'world'})
+
+@app.before_first_request
+def fetch_data_from_content_server():
+    #0. connect to content server and load its data to memory
+    def with_requests(url):
+        """Get a streaming response using requests library."""
+        import requests
+        return requests.get(url, stream=True)
+
+    content_server_url = "http://live-test-scores.herokuapp.com/scores"
+    response = with_requests(content_server_url)
+    client = sseclient.SSEClient(response)
+    for event in client.events():
+        print json.loads(event.data)
--- tests/test_application.py
+++ tests/test_application.py
@@ -7,10 +7,12 @@ class TestApplication(unittest.TestCase):
         application.app.testing = True
         self.app = application.app.test_client()
 
-    def test_students(self):
+    def test_ping(self):
         res = self.app.get('/students')
         self.assertEqual(res.data, '{"hello": "world"}')
 
-
+    # def test_fetch_data_from_content_server(self):
+    #     content_server_url = "http://live-test-scores.herokuapp.com/scores"
+    #     # self.app.fetch_data_from_content_server()
 if __name__ == '__main__':
     unittest.main()
