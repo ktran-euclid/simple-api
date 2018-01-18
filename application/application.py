4b5c5bb7dc4433c08f34d287a655517accf6df0c
Khoi Tran <ktran@euclidanalytics.com>
2018-01-17 23:35:57 -0800

    first test passed

--- application/application.py
+++ application/application.py
@@ -4,7 +4,7 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
-app = Flask(__name__) # create the application instance :)
+app = Flask('ScoreApplication') # create the application instance :)
 app.config.from_object(__name__) # load config from this file , application.py
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
