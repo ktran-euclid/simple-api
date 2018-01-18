a8154ae5f21fc87d2f37e5828eae1da8987ecaa8
Khoi Tran <ktran@euclidanalytics.com>
2018-01-18 04:13:45 -0800

    add test for student_profile

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,5 +1,6 @@
 import application
 import os
+from redis import Redis, StrictRedis
 import unittest
 
 class TestApplication(unittest.TestCase):
@@ -10,6 +11,12 @@ class TestApplication(unittest.TestCase):
     def test_ping(self):
         res = self.app.get('/ping')
         self.assertEqual(res.data, '{"hello": "world"}')
+
+    def test_student_profile(self):
+        students_score_cache = StrictRedis(host='localhost', port=6379, db=1)
+        students_score_cache.hset('test_student_id', 'exam_01', '9.0')
+        res = self.app.get('students/test_student_id')
+        self.assertEqual(res.data, '{"exam_01": "9.0"}')
 
 if __name__ == '__main__':
     unittest.main()
