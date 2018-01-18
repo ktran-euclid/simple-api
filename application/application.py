737093e1b939a394f0297046a956d9bf76160c55
Khoi Tran <ktran@euclidanalytics.com>
2018-01-18 00:53:46 -0800

    get celery working for background fetching

--- application/application.py
+++ application/application.py
@@ -1,5 +1,6 @@
 # all the imports
 
+from celery import Celery
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 import json
@@ -9,15 +10,32 @@ import sseclient
 
 app = Flask('ScoreApplication') # create the application instance :)
 app.config.from_object(__name__) # load config from this file , application.py
-
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+
+celery = make_celery(app)
 @app.route('/students', methods=['GET'])
 def ping():
     """Test if server is alive"""
     return json.dumps({'hello': 'world'})
 
-@app.before_first_request
+@celery.task
 def fetch_data_from_content_server():
     #0. connect to content server and load its data to memory
     def with_requests(url):
@@ -30,3 +48,7 @@ def fetch_data_from_content_server():
     client = sseclient.SSEClient(response)
     for event in client.events():
         print json.loads(event.data)
+
+@app.before_first_request
+def fetch_data():
+    fetch_data_from_content_server.delay()
