d2d14fd340ee115a83dd74c3f9a5e04db50455a9
Khoi Tran <ktran@euclidanalytics.com>
2018-01-17 23:21:49 -0800

    create a flaskr app

---
+++ application/application.py
@@ -0,0 +1,14 @@
+# all the imports
+import json
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__) # load config from this file , application.py
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.route('/students', methods=['GET'])
+def students():
+    return json.dumps({'hello': 'world'})
