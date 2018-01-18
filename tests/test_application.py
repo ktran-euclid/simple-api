6b47fb5d65a633966a0fb8ec22f8d7de4dd0c206
Khoi Tran <ktran@euclidanalytics.com>
2018-01-18 04:16:56 -0800

    test should have no fingerprint

--- tests/test_application.py
+++ tests/test_application.py
@@ -7,14 +7,17 @@ class TestApplication(unittest.TestCase):
     def setUp(self):
         application.app.testing = True
         self.app = application.app.test_client()
+        self.students_score_cache = StrictRedis(host='localhost', port=6379, db=1)
+        self.students_score_cache.hset('test_student_id', 'exam_01', '9.0')
+
+    def tearDown(self):
+        self.students_score_cache.delete('test_student_id')
 
     def test_ping(self):
         res = self.app.get('/ping')
         self.assertEqual(res.data, '{"hello": "world"}')
 
     def test_student_profile(self):
-        students_score_cache = StrictRedis(host='localhost', port=6379, db=1)
-        students_score_cache.hset('test_student_id', 'exam_01', '9.0')
         res = self.app.get('students/test_student_id')
         self.assertEqual(res.data, '{"exam_01": "9.0"}')
 
